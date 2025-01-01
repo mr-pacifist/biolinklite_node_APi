c9567e270ddc46d9a68b2d6dd93aa338e667657d
mr.piash.u@gmail.com <mr.piash.u@gmail.com>
2025-01-01 23:11:21 +0600

    CORS client upgraded

--- middlewares/common/setcookies.js
+++ middlewares/common/setcookies.js
@@ -15,10 +15,8 @@ const setCookies = function(req,res){
     maxAge: parseInt(process.env.JWT_EXPIRY),
     httpOnly: true,
     signed: true,
-    sameSite: 'Lax',
   });
   
-
 
   //user object check
   if(userObject.login){

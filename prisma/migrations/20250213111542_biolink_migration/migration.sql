ccd347229bc3407883439ebe0629840b8049ed55
mr.piash.u@gmail.com <mr.piash.u@gmail.com>
2025-02-13 17:16:38 +0600

    schema and migraion updated

--- prisma/migrations/20250213111542_biolink_migration/migration.sql
+++ prisma/migrations/20250213111542_biolink_migration/migration.sql
@@ -75,6 +75,8 @@ CREATE TABLE "ProfileCustomLink" (
 CREATE TABLE "Header" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Header_pkey" PRIMARY KEY ("id")
 );

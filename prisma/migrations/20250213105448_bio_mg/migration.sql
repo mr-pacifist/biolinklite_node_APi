7cdc884040823d2ebbaf7da594238c775b96309b
mr.piash.u@gmail.com <mr.piash.u@gmail.com>
2025-02-13 16:55:37 +0600

    migration updated

--- prisma/migrations/20250213105448_bio_mg/migration.sql
+++ prisma/migrations/20250213105448_bio_mg/migration.sql
@@ -56,6 +56,7 @@ CREATE TABLE "CustomLink" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "url" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "CustomLink_pkey" PRIMARY KEY ("id")
 );

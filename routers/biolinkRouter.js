589e31fce5df4dcb6a2856ce487e9846d912d520
Piash Uddin <149686649+mr-pacifist@users.noreply.github.com>
2025-03-31 14:41:01 +0600

    update profile changed

--- routers/biolinkRouter.js
+++ routers/biolinkRouter.js
@@ -8,7 +8,7 @@ const {getBiolink,getBiolinkByProfileId} = require("../controllers/biolinkContro
 // set retlimit
 const limiter = require("../middlewares/common/limiter");
 
-router.get("profile/:id([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})", limiter, getBiolinkByProfileId);
+router.get("/:id([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})", limiter, getBiolinkByProfileId);
 router.get("profile/:subDirectory",limiter, getBiolink );
 
 
